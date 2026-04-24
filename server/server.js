4c885875bf51e3242c4e1dcc25b150c5d45681dd
Sairaj <nagulasairaj16@gmail.com>
2026-04-24 23:17:37 +0530

    added test route

--- server/server.js
+++ server/server.js
@@ -12,6 +12,10 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
+app.get("/", (req, res) => {
+  res.send("LMS API is running...");
+});
+
 // MongoDB Connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("✅ MongoDB connected"))
