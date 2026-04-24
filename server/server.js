8e88b56535861638cf75e15488aaa6e98ddb2d61
Sairaj <nagulasairaj16@gmail.com>
2026-04-24 23:13:39 +0530

    fix: remove nodemon for production

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@ require("dotenv").config();
 
 const app = express();
 
+const PORT = process.env.PORT || 5000;
+
 // Middleware
 app.use(cors());
 app.use(express.json());
@@ -22,6 +24,6 @@ app.use("/api/quizzes", require("./routes/quiz"));
 app.use("/api/certificates", require("./routes/certificates"));
 
 // Start server (keep LAST)
-app.listen(process.env.PORT || 5000, () => {
-  console.log("✅ Server running");
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
