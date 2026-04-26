cb1768114d0f349f311b672262a0bc096ca8bbda
Sairaj <nagulasairaj16@gmail.com>
2026-04-26 19:27:42 +0530

    fix cloudinary upload

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -4,10 +4,11 @@ const cloudinary = require("../config/cloudinary");
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
+  params: async (req, file) => ({
     folder: "lms_uploads",
     resource_type: "auto",
-  },
+    public_id: Date.now() + "-" + file.originalname,
+  }),
 });
 
 const upload = multer({ storage });
