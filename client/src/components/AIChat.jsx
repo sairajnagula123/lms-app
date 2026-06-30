eb095ef9f2c6af5f0225fba360e79b6de02f6a50
Sairaj <nagulasairaj16@gmail.com>
2026-06-30 10:19:48 +0530

    langchain added css

--- client/src/components/AIChat.jsx
+++ client/src/components/AIChat.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
+import "../styles/AIChat.css";
 
 function AIChat({ courseTitle, onClose }) {
   const [question, setQuestion] = useState("");
